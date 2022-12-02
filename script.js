f752465af1d4c219ef331ecc0bde964c8e564e46
manishnepali <manish.nepali@student.ehb.be>
2022-12-03 00:09:03 +0100

    refactor is Checked function

--- script.js
+++ script.js
@@ -4,60 +4,76 @@ window.onload=()=>{
     const body = document.getElementById("content")
 
     const htmlString =`
-        <form action="">
-        <h4>Welke communicatie wenst u niet langer te ontvangen?</h4>
+    <p>Welke communicatie wenst u niet langer te ontvangen?</p>
+        <form action="" id="formBox">
         <input type="checkbox" id="callList" name="ckeckbox" value="checked">
         <label for="callList"> ik wens niet meer opgebeld te worden</label><br>
         <div id="infoText">
-        <p id="">
-        Bellen we u te vaak op? Geen probleem, 
-        we geven onze callcenters door dat u niet langer wenst opgebeld te worden.
-        U kunt u ook nog steeds registreren op de <a herf="#">bel-me-niet-meer-lijst</a>.
-        </p>
-        <input type="tel" id="phone" name="phone" placeholder="Telefoon" required><br>
-        <label id="invalid"> Gelieve een geldig nummer in te vullen</label><br>
-        <input type="submit" id="submitBtn" value="Submit">
+       
         </div>
+        
         </form>
         `
     body.innerHTML += htmlString;
+
     const infoText = document.getElementById("infoText");
     infoText.style.visibility = "hidden";
+
     const checkbox = document.getElementById("callList");
     checkbox.addEventListener("change", isBtnChecked);
-    const submitBtn = document.getElementById("submitBtn");
-    const invalid = document.getElementById("invalid");
-    invalid.style.visibility = "hidden";
-    submitBtn.disabled = true;
+
+
+
     function isBtnChecked(){
         if(this.checked){
             console.log('btn checked');
             infoText.style.visibility = "visible"        
+            formBox.style.borderColor = "gold"
+            infoText.innerHTML += `  
+            <p id="">
+            Bellen we u te vaak op? Geen probleem, 
+            we geven onze callcenters door dat u niet langer wenst opgebeld te worden.
+            U kunt u ook nog steeds registreren op de <a herf="#">bel-me-niet-meer-lijst</a>.
+            </p>
+            <input type="tel" id="phone" name="phone" placeholder="Telefoon" required><br>
+            <label id="invalid"> Gelieve een geldig nummer in te vullen</label><br>
+            <input type="submit" id="submitBtn" value="Submit">
+          `
+          const submitBtn = document.getElementById("submitBtn");
+          submitBtn.disabled = true;
+      
+          const invalid = document.getElementById("invalid");
+          invalid.style.visibility = "hidden";
+
+          const phone = document.getElementById("phone");
+          phone.addEventListener("keyup", numberValidation)
+      
+          //regular expression
+          let regex = /^\+32\d{8}$/;
+          function numberValidation(){
+              if(phone.value.match(regex)){
+                  console.log("ok");
+                  phone.style.borderColor = "green";
+                  invalid.style.visibility = "hidden"
+                  submitBtn.disabled = false;
+              }else{
+                  phone.style.borderColor = "red";
+                  console.log(phone.value);
+                  invalid.style.visibility = "visible"
+                  submitBtn.disabled = true;
+              }
+          
+          }
         }
         else{
             console.log('btn not checked');
             infoText.style.visibility = "hidden";
+            formBox.style.borderColor = "lightGrey"
+            infoText.innerHTML = "";
         }
 
     }
-    const phone = document.getElementById("phone");
-    phone.addEventListener("keyup", numberValidation)
-    //regular expression
-    let regex = /^\+32\d{8}$/;
-    function numberValidation(){
-        if(phone.value.match(regex)){
-            console.log("ok");
-            phone.style.borderColor = "green";
-            invalid.style.visibility = "hidden"
-            submitBtn.disabled = false;
-        }else{
-            phone.style.borderColor = "red";
-            console.log(phone.value);
-            invalid.style.visibility = "visible"
-            submitBtn.disabled = true;
-        }
-    
-    }
+
     // submitBtn.addEventListener("click", submitNumber);
     // function submitNumber(e){
     // e.preventDefault();
