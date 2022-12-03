c31cb6b102ac8a7c96ba512df03a0b8f2efb40ac
manishnepali <manish.nepali@student.ehb.be>
2022-12-03 01:50:49 +0100

    Added: documentations

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload=()=>{
     console.log("loaded")
 
     const body = document.getElementById("content");
-
+// form html string
     const htmlString =`
     <p>Welke communicatie wenst u niet langer te ontvangen?</p>
         <form action="" id="formBox">
@@ -15,15 +15,19 @@ window.onload=()=>{
         </form>
         `;
     body.innerHTML += htmlString;
-
+// form postion where info will be shown if user checks the box
     const infoText = document.getElementById("infoText");
     infoText.style.visibility = "hidden";
-
+// checkbox & evend listener onChange
     const checkbox = document.getElementById("callList");
     checkbox.addEventListener("change", isBtnChecked);
 
 
 
+/**
+ * The function checks if the checkbox is checked, if it is, it shows the infoText and adds the phone
+ * input field and submit button
+ */
     function isBtnChecked(){
         if(this.checked){
             console.log('btn checked');
@@ -41,15 +45,18 @@ window.onload=()=>{
             <label id="invalid"> Gelieve een geldig nummer in te vullen</label><br><br>
             <input type="submit" id="submitBtn" value="Submit">
           `;
+
+          //disable submit button until phone number is validated
           const submitBtn = document.getElementById("submitBtn");
           submitBtn.disabled = true;
-      
+          //if the phone number is invalid a messege will be shown under the input field
           const invalid = document.getElementById("invalid");
           invalid.style.visibility = "hidden";
-
+          // get phone number from input field, ever keyup it checks and validates
           const phone = document.getElementById("phone");
           phone.addEventListener("keyup", numberValidation)
           submitBtn.addEventListener("click", submitNumber);
+          // if number is valid user can submit number
           function submitNumber(e){
           e.preventDefault();
           const infoText = document.getElementById("infoText");
@@ -57,7 +64,8 @@ window.onload=()=>{
           infoText.innerHTML += `<p>U wordt niet meer opgebeld door onze callcenters. Dank u <br>
           uw telefoon nummer: ${phone.value}</p><br>`;
           }
-          //regular expression
+          //regular expression to validate belgain phone number
+          // always start with +32 "/^\+32\"" fallowed by 8 random digits
           let regex = /^\+32\d{8}$/;
           function numberValidation(){
               if(phone.value.match(regex)){
